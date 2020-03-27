bef48049ddb27b37ecced0770e286bea350a837a
Fynn Becker <post@fynn.be>
2020-03-27 12:18:27 +0100

    feat: Transpile CommonJS build for older browsers

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,30 @@
-module.exports = [
+import babel from 'rollup-plugin-babel'
+import { terser } from 'rollup-plugin-terser'
+
+export default [
   {
     input: 'src/main.js',
     output: {
       file: 'dist/iframe-click-to-play.esm.js',
-      format: 'esm'
-    }
+      format: 'esm',
+      sourcemap: true
+    },
+    plugins: [
+      terser()
+    ]
   },
   {
     input: 'src/main.js',
     output: {
       file: 'dist/iframe-click-to-play.common.js',
-      format: 'cjs'
-    }
+      format: 'cjs',
+      sourcemap: true
+    },
+    plugins: [
+      babel({
+        presets: ['@babel/env']
+      }),
+      terser()
+    ]
   }
 ]
