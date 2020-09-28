9dc42c788b1a81c3cd7a0056cd6249e48c303c9a
Fynn Becker <fynn@hey.com>
2020-09-28 08:54:24 +0200

    fix: Avoid browser history entry

--- src/main.js
+++ src/main.js
@@ -7,25 +7,33 @@ export class IframeClickToPlay {
    * @param {HTMLButtonElement} options.trigger Click-to-play trigger
    */
   constructor ({ iframe, fallback, trigger }) {
+    this.container = iframe
     this.iframe = iframe
+    this.src = iframe.dataset.src
     this.fallback = fallback
     this.trigger = trigger
+    this.placeholder = document.createComment('')
+
+    if (iframe.tagName !== 'IFRAME') {
+      const descendant = iframe.querySelector('iframe')
+      this.iframe = descendant
+      this.src = descendant.dataset.src
+    }
   }
 
   init () {
-    this.iframe.hidden = true
+    // Remove the iframe from the DOM to avoid a history entry on src change.
+    this.container.after(this.placeholder)
+    this.container.remove()
+
     this.trigger.addEventListener('click', () => this.play())
   }
 
   play () {
-    if (this.iframe.tagName === 'IFRAME') {
-      this.iframe.src = this.iframe.dataset.src
-    } else {
-      const descendant = this.iframe.querySelector('iframe')
-      descendant.src = descendant.dataset.src
-    }
-
-    this.iframe.hidden = false
+    // Set the iframe src before re-inserting the node into the DOM.
+    // This avoids a history entry due to a src change.
+    this.iframe.src = this.src
+    this.placeholder.replaceWith(this.container)
 
     this.fallback.remove()
     this.trigger.remove()
